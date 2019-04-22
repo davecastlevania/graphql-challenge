0d0541dc31e1c1b90aba46ef0316cecd8208de0c
David Castaneda <the.dave.castaneda@gmail.com>
2019-04-21 22:56:50 -0400

    successfully return of token

--- src/util/NSApi.js
+++ src/util/NSApi.js
@@ -1,59 +1,56 @@
 const url = 'https://api-dev.newstory.io/graphql';
 const apiKey = '0123456789abcdef0123456789abcdef';
-
+// Initial Access Token as Empty to Change Up Later
 let accessToken;
-
-
 /*
 ==================================
 ==== CURL REQUEST FOR SIGN IN ====
 ================================== 
-curl --location --request POST "https://api-dev.newstory.io/graphql" \
-  --header "ACCEPT: application/json" \
-  --header "X-Api-Key: {{api_key}}" \
-  --header "Content-Type: application/json" \
-  --data "{
-  \"query\": \"mutation {signInUser(email: \\\"{{email}}\\\", password:\\\"{{password}}\\\") { token viewer { uuid email firstName lastName } } }\",
-  \"variables\": null
+{
+  "query": "mutation {signInUser(email: \"{{email}}\", password:\"{{password}}\") { token viewer { uuid email firstName lastName } } }",
+  "variables": null
 }"
-*/
 
+===== SETUP QUERY for SIGN IN=====
+*/
 const signInQuery = (email, password) => {
-    return `
-    query {
-        mutation {
-            signInUser(email: "${email}", password: "${password}") {
-                token viewer {
-                    uuid
+    return {
+        "query": `mutation {
+            signInUser(email: \"${email}\", password:\"${password}\") { 
+                token viewer { 
+                    uuid 
                     email
                     firstName
-                    lastName
-                }
-            }
-        }
-        variables: null
-    }`
+                    lastName 
+                } 
+            } 
+        }`,
+         "variables": null
+  };
 }
-
 // Default Login Headers
 const headers = {
     method: "POST",
     headers: { "ACCEPT": "application/json", "X-Api-Key": apiKey, "Content-Type": "application/json"  }
 }
-
-
 const nsApi = {
 
     signIn(email, pw) {
-        console.log("You signed in right?", 'email: ', email)
         const creds = {body: JSON.stringify(signInQuery(email, pw))}
         const headerBody = {...headers, ...creds }
-        console.log(headerBody)
-        fetch(url, headerBody)
+         fetch(url, headerBody)
             .then(res => {
-                console.log(res)
-                return res
+                // Returns promise
+                return res.json()
             })
+            // Then passes returned promise values
+            // to accessToken variable
+            .then(obj => {
+                accessToken = obj.data.signInUser.token
+                console.log('Token storage', accessToken)
+
+            })
+            // Needs Error Handling
     },
 
     viewAllReceipients() {
